062453d60ed6321b328498400a0c226a85b88e64
Polina <ipolina364@gmail.com>
2025-05-21 20:51:30 +0300

    add files

---
+++ TI_LAB_4_1_git/TI_4/EDS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace TI_4;
+
+static class EDS
+{
+    static readonly BigInteger H0 = 100;
+
+    public static BigInteger HashFunction(byte[] textBytes, BigInteger bigIntegerR)
+    {
+        if (textBytes == null || textBytes.Length == 0)
+            return H0; // Возвращаем начальное значение хэша для пустого файла
+        
+        BigInteger result = RSA.QuickPowerMod(H0 + new BigInteger(textBytes[0]), 2, bigIntegerR);
+        
+        for (int i = 1; i < textBytes.Length; i++)
+        {
+            result = RSA.QuickPowerMod(result + new BigInteger(textBytes[i]), 2, bigIntegerR);
+        }
+        
+        return result;
+    }
+}
